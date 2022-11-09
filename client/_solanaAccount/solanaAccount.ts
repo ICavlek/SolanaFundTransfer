c8f7c60358d8f7623eb3821265927d2e656220b1
IvanC <ivan.cavlek1403@gmail.com>
2022-11-09 18:48:32 +0100

    Made it nice - Distributable

--- client/_solanaAccount/solanaAccount.ts
+++ client/_solanaAccount/solanaAccount.ts
@@ -1,36 +1,53 @@
 import { createKeypairFromFile } from "../_keypairFromFile/keypairFromFile";
-import { Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction, TransactionInstruction } from "@solana/web3.js";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction, TransactionInstruction } from "@solana/web3.js";
 
 export class SolanaAccount {
     name: string;
     keypair: Keypair;
+    connection: Connection;
+    programId: PublicKey;
 
-    constructor(name: string) {
+    constructor(name: string, connection: Connection, programId: PublicKey) {
         this.name = name;
         this.keypair = createKeypairFromFile(__dirname + `/../../accounts/${name}.json`);
+        this.connection = connection;
+        this.programId = programId;
     }
 
-    async sendLamports(connection: Connection, programId: PublicKey, to: PublicKey, amount: number) {
+    async requestAirdropSolana(amount: number) {
+        const latestBlockHash = await this.connection.getLatestBlockhash();
+        const airdropSignature = await this.connection.requestAirdrop(
+            this.keypair.publicKey,
+            amount * LAMPORTS_PER_SOL,
+        );
+        await this.connection.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: airdropSignature,
+        });
+    }
+
+    async sendSolana(to: SolanaAccount, amount: number) {
         console.log("Paul sends some SOL to George...");
         console.log(`   Paul's public key: ${this.keypair.publicKey}`);
-        console.log(`   George's public key: ${to}`);
+        console.log(`   George's public key: ${to.keypair.publicKey}`);
 
         let data = Buffer.alloc(8); // 8 bytes
         const lo = require("buffer-layout");
-        lo.ns64("value").encode(amount, data);
+        lo.ns64("value").encode(amount * LAMPORTS_PER_SOL, data);
 
         let ins = new TransactionInstruction({
             keys: [
                 { pubkey: this.keypair.publicKey, isSigner: true, isWritable: true },
-                { pubkey: to, isSigner: false, isWritable: true },
+                { pubkey: to.keypair.publicKey, isSigner: false, isWritable: true },
                 { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },
             ],
-            programId: programId,
+            programId: this.programId,
             data: data,
         })
 
         await sendAndConfirmTransaction(
-            connection,
+            this.connection,
             new Transaction().add(ins),
             [this.keypair]
         );
--- client/main.ts
+++ client/main.ts
@@ -1,72 +1,20 @@
 import {
     Connection,
-    Keypair,
-    LAMPORTS_PER_SOL,
     PublicKey,
-    sendAndConfirmTransaction,
-    SystemProgram,
-    Transaction,
-    TransactionInstruction,
 } from '@solana/web3.js';
 import { getConnection } from "./_connection/connection";
 import { getProgramID } from "./_solanaProgramKeyPair/solanaProgramKeyPair";
-import { createKeypairFromFile } from "./_keypairFromFile/keypairFromFile";
 import { SolanaAccount } from "./_solanaAccount/solanaAccount";
 
 async function main() {
 
     let connection: Connection = getConnection('devnet');
     let programId: PublicKey = getProgramID();
+    const accountPaul: SolanaAccount = new SolanaAccount("paul", connection, programId);
+    const accountGeorge: SolanaAccount = new SolanaAccount("george", connection, programId);
 
-    // Our sample members are Ringo, George, Paul & John.
-    let georgeKeypair: Keypair = createKeypairFromFile(__dirname + "/../accounts/george.json");
-
-    let accountPaul: SolanaAccount = new SolanaAccount("paul");
-    accountPaul.sendLamports(connection, programId, georgeKeypair.publicKey, 4000000);
-
-    // const latestBlockHash = await connection.getLatestBlockhash();
-
-    // const airdropSignaturePaul = await connection.requestAirdrop(
-    //     paulKeypair.publicKey,
-    //     LAMPORTS_PER_SOL,
-    // );
-
-    // const airdropSignatureJohn = await connection.requestAirdrop(
-    //     johnKeypair.publicKey,
-    //     LAMPORTS_PER_SOL,
-    // );
-
-    // We'll start by airdropping some lamports to Paul & John.
-    // await connection.confirmTransaction({
-    //     blockhash: latestBlockHash.blockhash,
-    //     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-    //     signature: airdropSignaturePaul,
-    // });
-
-    // await connection.confirmTransaction({
-    //     blockhash: latestBlockHash.blockhash,
-    //     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-    //     signature: airdropSignatureJohn,
-    // });
-
-    // Paul sends some SOL to George.
-    // console.log("Paul sends some SOL to George...");
-    // console.log(`   Paul's public key: ${paulKeypair.publicKey}`);
-    // console.log(`   George's public key: ${georgeKeypair.publicKey}`);
-    // await sendLamports(connection, programId, paulKeypair, georgeKeypair.publicKey, 4000000);
-
-    // // George sends some SOL over to John.
-    // console.log("George sends some SOL over to John...");
-    // console.log(`   George's public key: ${georgeKeypair.publicKey}`);
-    // console.log(`   John's public key: ${johnKeypair.publicKey}`);
-    // await sendLamports(georgeKeypair, johnKeypair.publicKey, 2000000);
-
-    // // John sends some SOL to Ringo.
-    // console.log("John sends some SOL to Ringo...");
-    // console.log(`   John's public key: ${johnKeypair.publicKey}`);
-    // console.log(`   Ringo's public key: ${ringoKeypair.publicKey}`);
-    // await sendLamports(johnKeypair, ringoKeypair.publicKey, 5000000);
-
+    // await accountPaul.requestAirdropSolana(1);
+    await accountPaul.sendSolana(accountGeorge, 0.4);
 }
 
 
