fb3c059180b6873ae2a800def90c707226149fee
IvanC <ivan.cavlek1403@gmail.com>
2022-11-09 20:02:36 +0100

    Fixed bugs in .sh script

--- _cicd/cicd.sh
+++ _cicd/cicd.sh
@@ -9,13 +9,13 @@ case $1 in
         for x in $(solana program show --programs | awk 'RP==0 {print $1}'); do 
             if [[ $x != "Program" ]]; 
             then 
-                solana program close $x;
+                solana program close $x --bypass-warning;
             fi
         done
         for program in "${SOLANA_PROGRAMS[@]}"; do
             cargo clean --manifest-path=./$program/Cargo.toml
         done
-        rm -rf _dist/.
+        rm -rf _dist/*
         ;;
     "clean")
         rm -rf ./node_modules
@@ -38,7 +38,7 @@ case $1 in
                 solana program close $x --bypass-warning; 
             fi
         done
-        rm -rf _dist/.
+        rm -rf _dist/*
         for program in "${SOLANA_PROGRAMS[@]}"; do
             cargo clean --manifest-path=./$program/Cargo.toml
             cargo build-sbf --manifest-path=./$program/Cargo.toml --sbf-out-dir=./_dist
