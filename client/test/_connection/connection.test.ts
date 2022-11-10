d4b8a7e79782956843d6fbb471f5a3f5a388b75f
IvanC <ivan.cavlek1403@gmail.com>
2022-11-10 20:06:17 +0100

    npm install -save-dev typescript ts-node @types/node
    npm install -save-dev jest ts-jest @types/jest

---
+++ client/test/_connection/connection.test.ts
@@ -0,0 +1,7 @@
+// import { getConnection } from "../../app/_connection/connection";
+
+describe('getConnection test suite', () => {
+    test('first test', () => {
+        console.log('tests work!!');
+    });
+});
