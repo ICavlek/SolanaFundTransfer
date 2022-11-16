8b9f1fed663c562727b5c01ee8ebe9f37f64bd83
IvanC <ivan.cavlek1403@gmail.com>
2022-11-16 17:28:56 +0100

    Modified connection test

--- client/test/_connection/connection.test.ts
+++ client/test/_connection/connection.test.ts
@@ -2,15 +2,18 @@ import { getConnection } from "../../app/_connection/connection";
 import { ClientBaseError } from "../../app/_error/error";
 
 describe('getConnection test suite', () => {
-    test('first test', () => {
-        try {
-            const connection = getConnection('mainnet');
-        } catch (e) {
-            if (e instanceof ClientBaseError) {
-                console.log(e.message);
-            } else {
-                throw e;
-            }
-        }
+    test('mainnet connection', () => {
+        getConnection('mainnet');
+    });
+
+    test('devnet connection', () => {
+        getConnection('mainnet');
+    });
+
+    test('wrong connection', () => {
+        const t = () => {
+            getConnection('dinamo');
+        };
+        expect(t).toThrow(ClientBaseError);
     });
 });
