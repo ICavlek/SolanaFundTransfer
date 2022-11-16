51c6bff5ffcbe872e0b307c1d88d0f540efb4495
IvanC <ivan.cavlek1403@gmail.com>
2022-11-16 18:04:14 +0100

    Small modification for condition

--- client/app/_connection/connection.ts
+++ client/app/_connection/connection.ts
@@ -2,10 +2,10 @@ import { Connection } from "@solana/web3.js";
 import { ConnectionError } from "../_error/error";
 
 export function getConnection(solanaNetwork: string): Connection {
-    if (solanaNetwork === 'devnet' || solanaNetwork === 'mainnet') {
-        return new Connection(
-            `https://api.${solanaNetwork}.solana.com`, 'confirmed'
-        );
+    if ((solanaNetwork !== 'devnet') && (solanaNetwork !== 'mainnet')) {
+        throw new ConnectionError();
     }
-    throw new ConnectionError();
+    return new Connection(
+        `https://api.${solanaNetwork}.solana.com`, 'confirmed'
+    );
 }
